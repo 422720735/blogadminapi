3568828ba1aaae737b8ae79698a87d3e6c70e9bb
422720735 <422720735@qq.com>
2019-10-08 22:08:06 +0800

    登陆验证

--- servers/login.go
+++ servers/login.go
@@ -17,13 +17,13 @@ import (
 func SeleltUsers(user, pwd string) (*model.User, error) {
 	// 查询用户，密码
 	// SELECT username, `password` from tb_user WHERE username = 'admin' and  `password` = 'e10adc3949ba59abbe56e057f20f883e';
-	stmtOut, err := dbops.DbConn.Prepare("select username, `password` from tb_user where username = ?")
+	stmtOut, err := dbops.DbConn.Prepare("select username, `password` from tb_user where username = ? and `password` = ?")
 	res := new(model.User)
 	if err != nil {
 		logs.Error("login is user err", err.Error())
 		return res, err
 	}
-	err = stmtOut.QueryRow(user).Scan(&res.Username, &res.Password)
+	err = stmtOut.QueryRow(&user, &pwd).Scan(&res.Username, &res.Password)
 	if err != nil {
 		logs.Error("select user err", err.Error())
 		return res, err
